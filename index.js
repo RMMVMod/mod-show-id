f49d4dc00449252c5a5fd7e172747eab86d52cba
Yeechan Lu <wz.bluesnow@gmail.com>
2015-11-30 23:35:14 +0800

    Use new api

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var ModAPI = require('modapi')
 var _ = require('lodash')
 
 var qml = ModAPI.QMLFile("Singletons/DataManager.qml")
-var nameOrId = _.find(qml.root.node.functions, {name: "nameOrId"})
 
-nameOrId.content = [
+qml.root.node.function("nameOrId", [
   'function nameOrId(name, id, numDigits) {',
   '    if (name.length) {',
   '        numDigits = numDigits || 4;',
@@ -14,6 +13,6 @@ nameOrId.content = [
   '        return "#" + makeIdText(id, numDigits);',
   '    }',
   '}',
-].join("\n");
+].join("\n"))
 
 qml.save()
